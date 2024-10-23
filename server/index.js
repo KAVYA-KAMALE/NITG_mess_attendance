6a25feba97cce17353e101ec2eec5249c239dc57
Kavya <133418422+KAVYA-KAMALE@users.noreply.github.com>
2024-10-23 18:23:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,44 +4,36 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
-const os = require('os');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors({
-    origin:'*'
+    origin: '*'
 }));
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({"Server Status":"Running"});
-})
+app.get("/", (req, res) => {
+    res.status(200).json({ "Server Status": "Running" });
+});
 
-// Serve static files from the uploads directory
-const uploadsDir = path.join(os.tmpdir(), 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
-app.use(express.static("uploads"));
+// Removed serving static files from the uploads directory
+
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
 const PORT = process.env.PORT || 4000;
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("ERR: ",error);
-        throw error
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error;
+        });
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
     })
-    app.listen(PORT,()=>{
-        console.log(`Server running on port ${PORT}`);
-    })
-})
-.catch((err)=>{
-    console.log("MongoDB connection error !!! ",err);
-})
-
+    .catch((err) => {
+        console.log("MongoDB connection error !!! ", err);
+    });
